34b78a0f7be03bff49eb2375e13153324d79c459
Oleksii Stepanyk <ostepanyk@kse.org.ua>
2024-04-07 02:25:44 +0300

    Added methods to Graph.cs

--- BoruvkaAlgorithm/BoruvkaAlgorithm/Graph.cs
+++ BoruvkaAlgorithm/BoruvkaAlgorithm/Graph.cs
@@ -8,25 +8,33 @@ namespace BoruvkaAlgorithm;
 /// and it can be connected to other vertices via edges. 
 /// Each vertex maintains a list of its neighboring vertices and edges.
 /// </remarks>
-
 public class Vertex
 {
     public string Name { get; set; }
-    public List<Vertex>? Neighbors { get; set; }
-    public List<Edge>? Edges { get; set; }
+    internal HashSet<Vertex> Neighbors { get; }
+    internal List<Edge> Edges { get; }
 
     public Vertex(string name)
     {
         Name = name;
-        Neighbors = new List<Vertex>();
+        Neighbors = new HashSet<Vertex>();
         Edges = new List<Edge>();
     }
 
-    public Vertex(string name, List<Vertex> neighbors, List<Edge> edges)
+    public Vertex(string name, HashSet<Vertex> neighbors)
     {
         Name = name;
         Neighbors = neighbors;
-        Edges = edges;
+        Edges = new List<Edge>();
+        CreateEdges();
+    }
+
+    private void CreateEdges()
+    {
+        foreach (var neighbor in Neighbors)
+        {
+            Edges.Add(new Edge(this, neighbor, 0));
+        }
     }
 }
 
@@ -37,7 +45,6 @@ public class Vertex
 /// An edge is a connection between two vertices in a graph.
 /// Each edge has a weight associated with it.
 /// </remarks>
-
 public class Edge
 {
     public Vertex Vertex1 { get; set; }
@@ -55,16 +62,20 @@ public class Edge
 
     private void AssignVertices()
     {
-        Vertex1.Neighbors?.Add(Vertex2);
-        Vertex2.Neighbors?.Add(Vertex1);
-        Vertex1.Edges?.Add(this);
-        Vertex2.Edges?.Add(this);
+        if (!Vertex1.Neighbors.Add(Vertex2))
+        {
+            return;
+        }
+
+        Vertex2.Neighbors.Add(Vertex1);
+        Vertex1.Edges.Add(this);
+        Vertex2.Edges.Add(this);
     }
 }
 
 public class Graph
 {
-    private List<Vertex> Vertices { get; }
+    private HashSet<Vertex> Vertices { get; }
     private List<Edge> Edges { get; }
 
     public Graph(Vertex[] vertices, Edge[] edges)
@@ -75,7 +86,7 @@ public class Graph
         Array.ForEach(edges, e => Edges.Add(e));
     }
 
-    public Graph(List<Vertex> vertices, List<Edge> edges)
+    public Graph(HashSet<Vertex> vertices, List<Edge> edges)
     {
         Vertices = vertices;
         Edges = edges;
@@ -88,7 +99,7 @@ public class Graph
         Edges = new();
     }
 
-    public Graph(List<Vertex> vertices)
+    public Graph(HashSet<Vertex> vertices)
     {
         Vertices = vertices;
         Edges = new();
@@ -100,23 +111,116 @@ public class Graph
         Edges = new();
     }
 
+    public bool Adjacent(Vertex vertex1, Vertex vertex2)
+    {
+        foreach (var edge in Edges)
+        {
+            if ((edge.Vertex1 == vertex1 && edge.Vertex2 == vertex2) ||
+                (edge.Vertex1 == vertex2 && edge.Vertex2 == vertex1))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public HashSet<Vertex> GetNeighbors(Vertex vertex)
+    {
+        return vertex.Neighbors;
+    }
+
     public void AddVertex(Vertex vertex)
     {
-        Vertices.Add(vertex);
+        if (Vertices.Add(vertex)) return;
+        Console.WriteLine("Error. Vertex already exists in the graph.");
+    }
+
+    public void RemoveVertex(Vertex vertex)
+    {
+        if (!Vertices.Contains(vertex))
+        {
+            Console.WriteLine("Error. Vertex does not exist in the graph.");
+            return;
+        }
+
+        foreach (var edge in Edges)
+        {
+            if (edge.Vertex1 == vertex || edge.Vertex2 == vertex)
+            {
+                Edges.Remove(edge);
+            }
+        }
+
+        Vertices.Remove(vertex);
     }
 
     public void AddEdge(Edge edge)
     {
         if (!Vertices.Contains(edge.Vertex1) || !Vertices.Contains(edge.Vertex2))
         {
-            throw new ArgumentException("The edge must connect two vertices in the graph.");
+            Console.WriteLine("Error. One or both vertices are not in the graph.");
+            return;
         }
 
         Edges.Add(edge);
     }
 
-    public List<Vertex> GetNeighbors(Vertex vertex)
+    public void DeleteEdge(Edge edge)
     {
-        return vertex.Neighbors ?? new();
+        if (!Edges.Contains(edge))
+        {
+            Console.WriteLine("Error. Edge is not in the graph.");
+            return;
+        }
+
+        Edges.Remove(edge);
+    }
+
+    public List<List<Vertex>> AdjacencyList()
+    {
+        var adjacencyList = new List<List<Vertex>>();
+        foreach (var vertex in Vertices)
+        {
+            var neighbors = new List<Vertex>();
+            foreach (var neighbor in vertex.Neighbors)
+            {
+                neighbors.Add(neighbor);
+            }
+
+            adjacencyList.Add(neighbors);
+        }
+
+        return adjacencyList;
+    }
+
+
+    public int[,] AdjacencyMatrix()
+    {
+        var adjacencyMatrix = new int[Vertices.Count, Vertices.Count];
+        var vertexList = Vertices.ToList();
+        for (var i = 0; i < Vertices.Count; i++)
+        {
+            for (var j = 0; j < Vertices.Count; j++)
+            {
+                adjacencyMatrix[i, j] = FindEdgeWeight(vertexList[i], vertexList[j]);
+            }
+        }
+
+        return adjacencyMatrix;
+    }
+
+    private int FindEdgeWeight(Vertex vertex1, Vertex vertex2)
+    {
+        foreach (var edge in Edges)
+        {
+            if ((edge.Vertex1 == vertex1 && edge.Vertex2 == vertex2) ||
+                (edge.Vertex1 == vertex2 && edge.Vertex2 == vertex1))
+            {
+                return edge.Weight;
+            }
+        }
+
+        return 0;
     }
 }
