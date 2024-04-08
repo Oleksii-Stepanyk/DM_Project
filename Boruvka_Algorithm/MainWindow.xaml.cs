2eb798bbbca443185fb82acf15ffb09cfef7c137
Oleksii Stepanyk <ostepanyk@kse.org.ua>
2024-04-08 10:54:19 +0300

    Changed solution to WPF App

---
+++ Boruvka_Algorithm/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Boruvka_Algorithm;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
